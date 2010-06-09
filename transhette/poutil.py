0c4caf3bf4d328a128663971111e5105f11bfea9
esanchez <esanchez@43d3a112-9a52-0410-bc29-92ccca371478>
2010-06-09 09:23:35 +0000

    Create po list in the same way django update its translation catalog. Fixes #100

--- transhette/poutil.py
+++ transhette/poutil.py
@@ -1,4 +1,7 @@
-import re, sys, os
+import re
+import sys
+import os
+
 from django.conf import settings
 from transhette import polib
 try:
@@ -9,32 +12,18 @@ except NameError:
 
 def find_pos(lang, include_djangos=False, include_transhette=False):
     """
-    scans a couple possible repositories of gettext catalogs for the given 
+    scans a couple possible repositories of gettext catalogs for the given
     language code
 
     """
 
     paths = []
 
-    # project/locale
-    parts = settings.SETTINGS_MODULE.split('.')
-    project = __import__(parts[0], {}, {}, [])
-    paths.append(os.path.join(os.path.dirname(project.__file__), 'locale'))
-
-    # django/locale
-    if include_djangos:
-        paths.append(os.path.join(os.path.dirname(sys.modules[settings.__module__].__file__), 'locale'))
-
-    # settings 
-    for localepath in settings.LOCALE_PATHS:
-        if os.path.isdir(localepath):
-            paths.append(localepath)
-
     # project/app/locale
-    for appname in settings.INSTALLED_APPS:
+    for appname in reversed(settings.INSTALLED_APPS):
         if 'transhette' == appname and include_transhette == False:
             continue
-        appname = str(appname) # to avoid a fail in __import__ sentence 
+        appname = str(appname) # to avoid a fail in __import__ sentence
         p = appname.rfind('.')
         if p >= 0:
             app = getattr(__import__(appname[:p], {}, {}, [appname[p+1:]]), appname[p+1:])
@@ -45,40 +34,62 @@ def find_pos(lang, include_djangos=False, include_transhette=False):
 
         if os.path.isdir(apppath):
             paths.append(apppath)
-            
-    ret = set()
+
+    # project/locale
+    parts = settings.SETTINGS_MODULE.split('.')
+    project = __import__(parts[0], {}, {}, [])
+    paths.append(os.path.join(os.path.dirname(project.__file__), 'locale'))
+
+    # settings
+    for localepath in settings.LOCALE_PATHS:
+        if os.path.isdir(localepath):
+            paths.append(localepath)
+
+    # django/locale
+    if include_djangos:
+        paths.append(os.path.join(os.path.dirname(sys.modules[settings.__module__].__file__), 'locale'))
+
+    ret = []
     rx=re.compile(r'(\w+)/../\1')
-    langs = (lang,)
+    langs = (lang, )
     if u'-' in lang:
-        _l,_c =  map(lambda x:x.lower(),lang.split(u'-'))
+        _l, _c = map(lambda x: x.lower(), lang.split(u'-'))
         langs += (u'%s_%s' %(_l, _c), u'%s_%s' %(_l, _c.upper()), )
     elif u'_' in lang:
-        _l,_c = map(lambda x:x.lower(),lang.split(u'_'))
+        _l, _c = map(lambda x: x.lower(), lang.split(u'_'))
         langs += (u'%s-%s' %(_l, _c), u'%s-%s' %(_l, _c.upper()), )
-        
+
     for path in paths:
         for lang_ in langs:
-            dirname = rx.sub(r'\1', '%s/%s/LC_MESSAGES/' %(path,lang_))
-            for fn in ('django.po','djangojs.po',):
-                if os.path.isfile(dirname+fn):
-                    ret.add(os.path.abspath(dirname+fn))
-    return list(ret)
+            dirname = rx.sub(r'\1', '%s/%s/LC_MESSAGES/' %(path, lang_))
+            for fn in ('django.po', 'djangojs.po', ):
+                if os.path.isfile(dirname+fn) and os.path.abspath((dirname+fn)) not in ret:
+                    ret.append(os.path.abspath(dirname+fn))
+    return ret
 
-def pagination_range(first,last,current):
+
+def pagination_range(first, last, current):
     r = []
-    
+
     r.append(first)
-    if first + 1 < last: r.append(first+1)
-    
-    if current -2 > first and current -2 < last: r.append(current-2)
-    if current -1 > first and current -1 < last: r.append(current-1)
-    if current > first and current < last: r.append(current)
-    if current + 1 < last and current+1 > first: r.append(current+1)
-    if current + 2 < last and current+2 > first: r.append(current+2)
-    
-    if last-1 > first: r.append(last-1)
+    if first + 1 < last:
+        r.append(first+1)
+
+    if current -2 > first and current -2 < last:
+        r.append(current-2)
+    if current -1 > first and current -1 < last:
+        r.append(current-1)
+    if current > first and current < last:
+        r.append(current)
+    if current + 1 < last and current+1 > first:
+        r.append(current+1)
+    if current + 2 < last and current+2 > first:
+        r.append(current+2)
+
+    if last-1 > first:
+        r.append(last-1)
     r.append(last)
-    
+
     r = list(set(r))
     r.sort()
     prev = 10000
@@ -92,6 +103,7 @@ def pagination_range(first,last,current):
     return r
 
 FUZZY = 'fuzzy'
+
 
 def priority_merge(po_destination, po_source, priority=False):
     for entry in po_source:
